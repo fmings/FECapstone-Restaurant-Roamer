fe6d564bd554fdc439a197465c9528ee7c74ab58
fmings <mingsfelicia@gmail.com>
2024-05-20 20:48:17 -0500

    set up view restaurant cards

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { Card } from 'react-bootstrap';
-import { getRestaurants } from '../api/restaurantData';
+// import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+// import { getRestaurants } from '../api/restaurantData';
 
-export default function RestaurantCard() {
-  const [restaurants, setRestaurants] = useState({});
-
-  useEffect(() => {
-    getRestaurants().then(setRestaurants);
-  }, []);
-
+export default function RestaurantCard({ restaurantObj }) {
   return (
-    <Card className="object-card" style={{ width: '18rem' }}>
-      <Card.Img className="member-card-image" variant="top" src="https://images.squarespace-cdn.com/content/v1/5fd1468dc461dc1c6e53de4d/c8de2b29-1c37-470d-b3fb-7ed1f69539ae/Script_logo+White.png?format=1500w}" />
-      <Card.Body>
-        <Card.Title className="bold">Test</Card.Title>
-        {restaurants.map((restaurant) => (
-          <Card.Title>{restaurant.name}</Card.Title>
-        ))}
-        <Card.Text>Test</Card.Text>
-        {/* {memberObj.uid === user.uid
-          ? (
-            <>
-              <Link href={`/member/edit/${memberObj.firebaseKey}`} passHref>
-                <Button className="button" variant="secondary">Edit</Button>
-              </Link>
-              <Button className="button" variant="danger" onClick={deleteThisMember}>Delete</Button>
-            </>
-          )
-          : ''} */}
-      </Card.Body>
-    </Card>
+    <>
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">Products</h2>
+
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+            <img
+              src={restaurantObj.logo}
+              alt="logo"
+              className="h-full w-full object-cover object-center group-hover:opacity-75"
+            />
+          </div>
+          <h3 className="mt-4 text-sm text-gray-700">{restaurantObj.name}</h3>
+        </div>
+      </div>
+    </>
   );
 }
+
+RestaurantCard.propTypes = {
+  restaurantObj: PropTypes.shape({
+    firebaseKey: PropTypes.string,
+    createdBy: PropTypes.string,
+    userList: PropTypes.bool,
+    name: PropTypes.string,
+    logo: PropTypes.string,
+    cuisineId: PropTypes.string,
+    tried: PropTypes.string,
+  }).isRequired,
+};
