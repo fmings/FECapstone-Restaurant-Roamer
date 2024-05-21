897b49a821d70429ef6f5ce62d3354a7858b09df
fmings <mingsfelicia@gmail.com>
2024-05-20 20:18:57 -0500

    set up restaurant card component

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card } from 'react-bootstrap';
+import { getRestaurants } from '../api/restaurantData';
 
 export default function RestaurantCard() {
+  const [restaurants, setRestaurants] = useState({});
+
+  useEffect(() => {
+    getRestaurants().then(setRestaurants);
+  }, []);
+
   return (
-    <div>
-      Restaurant Card
-    </div>
+    <Card className="object-card" style={{ width: '18rem' }}>
+      <Card.Img className="member-card-image" variant="top" src="https://images.squarespace-cdn.com/content/v1/5fd1468dc461dc1c6e53de4d/c8de2b29-1c37-470d-b3fb-7ed1f69539ae/Script_logo+White.png?format=1500w}" />
+      <Card.Body>
+        <Card.Title className="bold">Test</Card.Title>
+        {restaurants.map((restaurant) => (
+          <Card.Title>{restaurant.name}</Card.Title>
+        ))}
+        <Card.Text>Test</Card.Text>
+        {/* {memberObj.uid === user.uid
+          ? (
+            <>
+              <Link href={`/member/edit/${memberObj.firebaseKey}`} passHref>
+                <Button className="button" variant="secondary">Edit</Button>
+              </Link>
+              <Button className="button" variant="danger" onClick={deleteThisMember}>Delete</Button>
+            </>
+          )
+          : ''} */}
+      </Card.Body>
+    </Card>
   );
 }
--- pages/restaurant/allRestaurants.js
+++ pages/restaurant/allRestaurants.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import RestaurantCard from '../../components/RestaurantCard';
 
 export default function AllRestaurants() {
   return (
     <div>
-      All Restaurants
+      <h1 className="text-3xl font-bold underline">
+        All Restaurants
+      </h1>
+      <RestaurantCard />
     </div>
   );
 }
