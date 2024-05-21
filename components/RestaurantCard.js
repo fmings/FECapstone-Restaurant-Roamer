da41f13210a1605e7ac6e7a5143229a0b5f64a62
fmings <mingsfelicia@gmail.com>
2024-05-20 21:13:21 -0500

    added restaurant details to card

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,24 +1,46 @@
 // import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { Card } from 'react-bootstrap';
+import getNeighborhoods from '../api/neighborhoodData';
+import getCuisines from '../api/cuisineData';
 // import { getRestaurants } from '../api/restaurantData';
 
 export default function RestaurantCard({ restaurantObj }) {
+  const [neighborhoods, setNeighborhoods] = useState([]);
+  const [cuisines, setCuisines] = useState([]);
+
+  useEffect(() => {
+    getNeighborhoods().then(setNeighborhoods);
+    getCuisines().then(setCuisines);
+  }, []);
+
   return (
     <>
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
-
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img
-              src={restaurantObj.logo}
-              alt="logo"
-              className="h-full w-full object-cover object-center group-hover:opacity-75"
-            />
-          </div>
-          <h3 className="mt-4 text-sm text-gray-700">{restaurantObj.name}</h3>
+      <Card className="object-card" style={{ width: '18rem' }}>
+        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+          <img
+            src={restaurantObj.logo}
+            alt="logo"
+            className="h-full w-full object-cover object-center group-hover:opacity-75"
+          />
         </div>
-      </div>
+        <Card.Body>
+          <Card.Title>{restaurantObj.name}</Card.Title>
+          <Card.Text>Neighborhood: </Card.Text>
+          {neighborhoods.map((neighborhood) => (
+            <Card.Text>
+              {restaurantObj.neighborhoodId === neighborhood.firebaseKey ? neighborhood.name : ''}
+            </Card.Text>
+          ))}
+          <Card.Text>Cuisine: </Card.Text>
+          {cuisines.map((cuisine) => (
+            <Card.Text>
+              {restaurantObj.cuisineId === cuisine.firebaseKey ? cuisine.type : ''}
+            </Card.Text>
+          ))}
+        </Card.Body>
+      </Card>
     </>
   );
 }
@@ -30,6 +52,7 @@ RestaurantCard.propTypes = {
     userList: PropTypes.bool,
     name: PropTypes.string,
     logo: PropTypes.string,
+    neighborhoodId: PropTypes.string,
     cuisineId: PropTypes.string,
     tried: PropTypes.string,
   }).isRequired,
